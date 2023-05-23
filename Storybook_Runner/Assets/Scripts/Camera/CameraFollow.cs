1aa0ffcdc8ea76cba8c2f092fa8c685fbd33ca32
Jose Miguel Villafranca <gearshead145778@yahoo.com>
2023-05-22 23:00:56 -0500

    Camera follows player - Added "Waddle" animation that could be used to stimulate movement for player

---
+++ Storybook_Runner/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target = null;
+
+    private Vector3 offset;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, 0, target.position.z) + offset, Time.deltaTime * 3);
+    }
+}
